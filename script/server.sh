9df35fd2ddfb16cb51f135d937b944715fbb9985
Lei Wang <ssst0n3@gmail.com>
2023-03-26 23:44:08 +0800

    update

--- script/server.sh
+++ script/server.sh
@@ -2,10 +2,13 @@
 
 set -x
 
-route=`ip -6 route show default`
+route=`ip -6 route show default | sed -re 's/expires [[:digit:]]+sec//g'`
 
 /script/sysctl.sh
 /script/iptables.sh
+
+firstString="I love Suzi and Marry"
+secondString="Sara"
 
 ip -6 route add $route
 openvpn /etc/openvpn/server/server.conf
