5678054da4aac6218876aaef2c9734abb43f7545
Lei Wang <ssst0n3@gmail.com>
2023-03-26 22:32:26 +0800

    update

--- script/sysctl.sh
+++ script/sysctl.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-sysctl -w /proc/sys/net/ipv4/ip_forward=1
+sysctl -w net.ipv4.ip_forward=1
 sysctl -w net.ipv6.conf.all.disable_ipv6=0
 sysctl -w net.ipv6.conf.default.forwarding=1
 sysctl -w net.ipv6.conf.all.forwarding=1
